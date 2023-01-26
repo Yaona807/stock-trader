c617717ae186071d7ed964270f2810ee0bc3e4cc
i-708 <naoya.inoue0830@gmail.com>
2023-01-27 00:21:06 +0900

    移動平均線を分析する処理を追加

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main():
   # print('URL取得中')
   # chart_url = SBI.getChartImageURL(stock_code)
   # SBI.close()
-  chart_url = 'https://chart.iris.sbisec.co.jp/sbi/as/Mchart-mchart.html?ricCode=6696.T&type=real&hash=266ba144777201a58f8c8705342a7b2ab5c64612&size=0&mode=1&DaysNum=1&main=L&addon=SMA3&sub=None&TP=0&side=0&exdvMark=0&param1=25&param2=75&param3=200&diffquote=None&rand=1674571048740'
+  chart_url = 'https://chart.iris.sbisec.co.jp/sbi/as/Mchart-mchart.html?ricCode=3407.T&type=real&hash=bafb2b017ec5bce84e3983f79bc46089bd0476fe&size=0&mode=1&DaysNum=1&main=L&addon=SMA3&sub=None&TP=0&side=0&exdvMark=0&param1=25&param2=75&param3=200&diffquote=None&rand=1674746104393'
   CA = chartAnalysis()
   CA.readImageForUrl(chart_url)
   CA.analyze()
