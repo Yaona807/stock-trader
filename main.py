1e0d001cea5d8c17672a198bef736b883271b3b2
i-708 <naoya.inoue0830@gmail.com>
2023-01-28 22:41:43 +0900

    資産を取得する処理を追加

--- main.py
+++ main.py
@@ -10,17 +10,19 @@ def main():
   pass_word = settings.pass_word
   stock_code = settings.stock_code
 
-  # SBI = SBIController()
+  SBI = SBIController()
 
-  # print('ログイン中')
-  # SBI.login(user_id, pass_word)
+  print('ログイン中')
+  SBI.login(user_id, pass_word)
+  print('資産状況を取得中')
+  print(SBI.getAssetsHeld())
   # print('URL取得中')
   # chart_url = SBI.getChartImageURL(stock_code)
   # SBI.close()
-  chart_url = 'https://chart.iris.sbisec.co.jp/sbi/as/Mchart-mchart.html?ricCode=3407.T&type=real&hash=bafb2b017ec5bce84e3983f79bc46089bd0476fe&size=0&mode=1&DaysNum=1&main=L&addon=SMA3&sub=None&TP=0&side=0&exdvMark=0&param1=25&param2=75&param3=200&diffquote=None&rand=1674746104393'
-  CA = chartAnalysis()
-  CA.readImageForUrl(chart_url)
-  CA.analyze()
+  # chart_url = 'https://chart.iris.sbisec.co.jp/sbi/as/Mchart-mchart.html?ricCode=3407.T&type=real&hash=bafb2b017ec5bce84e3983f79bc46089bd0476fe&size=0&mode=1&DaysNum=1&main=L&addon=SMA3&sub=None&TP=0&side=0&exdvMark=0&param1=25&param2=75&param3=200&diffquote=None&rand=1674746104393'
+  # CA = chartAnalysis()
+  # CA.readImageForUrl(chart_url)
+  # CA.analyze()
 
 if __name__ == '__main__':
   main()
